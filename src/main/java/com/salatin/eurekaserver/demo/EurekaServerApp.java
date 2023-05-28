633eea58c181284b4ef7564f7cd774535a6157d0
Serhii Salatin <lifenjoy8888@gmail.com>
2023-05-28 14:07:58 +0300

    added Dockerfile and refactored files structure

--- src/main/java/com/salatin/eurekaserver/demo/EurekaServerApp.java
+++ src/main/java/com/salatin/eurekaserver/demo/EurekaServerApp.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DemoApplication {
+public class EurekaServerApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EurekaServerApp.class, args);
 	}
 
 }
